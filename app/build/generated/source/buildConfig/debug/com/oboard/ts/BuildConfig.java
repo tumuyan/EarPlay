1bb7ef32d683d4832b9c18e6b1a51aab76b6d483
Yazii <yazii@msn.cn>
2018-09-08 17:17:24 +0800

    2.0 发布。增加智能外放/智能听筒。增加距离传感器的阈值设定。增加音量键的功能。

--- app/build/generated/source/buildConfig/debug/com/oboard/ts/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/oboard/ts/BuildConfig.java
@@ -5,9 +5,9 @@ package com.oboard.ts;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.oboard.ts";
+  public static final String APPLICATION_ID = "com.tumuyan.earplay";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 9;
-  public static final String VERSION_NAME = "1.5";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "2.0";
 }
--- app/src/main/java/com/oboard/ts/MainActivity.java
+++ app/src/main/java/com/oboard/ts/MainActivity.java
@@ -17,13 +17,17 @@ import android.hardware.SensorManager;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
+
 import com.oboard.ts.R;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -43,6 +47,31 @@ public class MainActivity extends Activity implements SensorEventListener {
     SeekBar mVolumeBar;
     Sensor mSensor;//传感器
 
+
+    RadioButton RadioButton1;
+    RadioButton RadioButton2;
+    RadioButton RadioButton3;
+    SeekBar CloseSeekBar0;
+    SeekBar CloseSeekBar1;
+    TextView CloseText0;
+    TextView CloseText1;
+    TextView CloseText2;
+    TextView VolueText;
+    int close1=70;
+    float close2=0;
+    int close0=40;
+
+    CheckBox CloseEar0;
+    Boolean ear0=true;
+    CheckBox CloseEar1;
+    Boolean ear1=true;
+    CheckBox CloseEar2;
+    Boolean ear2=false;
+    CheckBox CloseEar3;
+    Boolean ear3=true;
+
+
+
     int maxVolume, systemVolume;
 
     @Override
@@ -50,15 +79,50 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        ii = findViewById(R.id.i);
+        RadioButton1=(RadioButton)findViewById(R.id.radioButton1);
+        RadioButton2=(RadioButton)findViewById(R.id.radioButton2);
+        RadioButton3=(RadioButton)findViewById(R.id.radioButton3);
+        CloseSeekBar0=(SeekBar)findViewById(R.id.closeSeekBar0);
+        CloseSeekBar1=(SeekBar)findViewById(R.id.closeSeekBar1);
+        CloseText0=(TextView)findViewById(R.id.closeText0);
+        CloseText1=(TextView)findViewById(R.id.closeText1);
+        CloseText2=(TextView)findViewById(R.id.closeText2);
+        VolueText=(TextView)findViewById(R.id.volueText);
+        CloseEar0=(CheckBox)findViewById(R.id.closeEar0);
+        CloseEar1=(CheckBox)findViewById(R.id.closeEar1);
+        CloseEar2=(CheckBox)findViewById(R.id.closeEar2);
+        CloseEar3=(CheckBox)findViewById(R.id.closeEar3);
+        //    ii = findViewById(R.id.i);
         cb = (CheckBox)findViewById(R.id.mainCheckBox1);
         mVolumeBar = (SeekBar)findViewById(R.id.mainSeekBar1);
+
+        CloseEar0.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener(){
+            public void onCheckedChanged(CompoundButton view, boolean state) {
+                ear0=state;
+            }
+        });
+        CloseEar1.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener(){
+            public void onCheckedChanged(CompoundButton view, boolean state) {
+                ear1=state;
+            }
+        });
+        CloseEar2.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener(){
+            public void onCheckedChanged(CompoundButton view, boolean state) {
+                ear2=state;
+            }
+        });
+        CloseEar3.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener(){
+            public void onCheckedChanged(CompoundButton view, boolean state) {
+                ear3=state;
+            }
+        });
 
         cb.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener() {
                 public void onCheckedChanged(CompoundButton view, boolean state) {
                     if (state) {
                         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
                         mSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
 
                         //息屏设置
                         mPowerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
@@ -89,11 +153,13 @@ public class MainActivity extends Activity implements SensorEventListener {
         mVolumeBar.setMax(maxVolume);
         //设置为系统现在的音量
         mVolumeBar.setProgress(systemVolume);
+        VolueText.setText(""+systemVolume);
         mVolumeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                 @Override
                 public void onProgressChanged(SeekBar sb, int p, boolean b) {
                     systemVolume = p;
                     audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, systemVolume, 0);
+                    VolueText.setText(""+p);
                 }
                 public void onStartTrackingTouch(SeekBar sb) {}
                 public void onStopTrackingTouch(SeekBar sb) {}
@@ -113,6 +179,48 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         mName = getResources().getString(R.string.a);
         setNotification();
+
+
+        CloseSeekBar0.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                CloseText0.setText(""+i/10.0);
+                close0=i;
+                if(close1<close0){
+                    CloseSeekBar1.setProgress(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        CloseSeekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                CloseText1.setText(""+i/10.0);
+                close1=i;
+                if(close1<close0){
+                    CloseSeekBar0.setProgress(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     /**
@@ -122,20 +230,34 @@ public class MainActivity extends Activity implements SensorEventListener {
      */
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.values[0] == 0.0) {
+
+        CloseText2.setText(""+event.values[0]);
+        close2=event.values[0];
+        Log.i("close2",""+close0/event.values[0]);
+        if (close0/event.values[0]  >= 10 ) {
+            if(ear0){
+                if(ear2){
+                    onModeChange(2);
+                }else {
+                    onModeChange(3);}
+            }
             //贴近手机
             //关闭屏幕
-            if (!mWakeLock.isHeld())
+            if (!mWakeLock.isHeld() && ear3)
                 mWakeLock.acquire();
 
-        } else {
+        } else if(close1/event.values[0]<10) {
             //离开手机
             //唤醒设备
-            if (mWakeLock.isHeld())
+            if(ear1){
+
+                onModeChange(0);
+            }
+            if (mWakeLock.isHeld() && ear3)
                 mWakeLock.release();
         }
     }
-    
+
     int num = -1;
     @Override
     protected void onNewIntent(Intent intent) {
@@ -148,7 +270,7 @@ public class MainActivity extends Activity implements SensorEventListener {
             };
             
             new AlertDialog.Builder(this)
-                .setTitle("EarPlay")
+                .setTitle(getString(R.string.name))
                 .setIcon(R.mipmap.i)
                 .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
@@ -190,11 +312,45 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
     @Override
+
     public boolean onKeyDown(int keyCode, KeyEvent event) {
+
         if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
             moveTaskToBack(true);
+            return true;
+        }else if(keyCode==KeyEvent.KEYCODE_VOLUME_DOWN){
+            if(systemVolume>0){
+                systemVolume--;
+      mVolumeBar.setProgress(systemVolume);
+            }
+
+
+            return true;
         }
-        return super.onKeyDown(keyCode, event);
+        else if(keyCode==KeyEvent.KEYCODE_VOLUME_UP)
+        {
+            if(systemVolume<maxVolume){
+                systemVolume++;
+                mVolumeBar.setProgress(systemVolume);
+            }
+            return true;
+        }
+        else return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if(keyCode==KeyEvent.KEYCODE_VOLUME_DOWN){
+            Log.i("按键松开","+");
+            return true;
+        }
+        else if(keyCode==KeyEvent.KEYCODE_VOLUME_UP)
+        {
+            Log.i("按键松开","-");
+            return true;
+        }
+        else return super.onKeyUp(keyCode, event);
     }
 
     public void onModeChange(View v) {
@@ -211,7 +367,27 @@ public class MainActivity extends Activity implements SensorEventListener {
             getResources().getString(R.string.b),
             getResources().getString(R.string.c)
         }[y[i]];
-        mbar(ii.getHeight() * y[i]);//指示条
+
+        Log.i("onChange",mName);
+
+        switch (i){
+            case 0:
+                RadioButton1.toggle();
+                Log.i("onChange",""+i);
+                break;
+            case 3:
+                RadioButton2.toggle();
+                Log.i("onChange",""+i);
+                break;
+            case 2:
+                RadioButton3.toggle();
+                Log.i("onChange",""+i);
+                break;
+
+        }
+
+
+    //    mbar(ii.getHeight() * y[i]);//指示条
         cancelNotification();//删除通知
         setNotification();//显示通知
     }
@@ -228,8 +404,8 @@ public class MainActivity extends Activity implements SensorEventListener {
         switch (item.getItemId()) {
             case R.id.gy:
                 new AlertDialog.Builder(this)
-                    .setTitle("关于")
-                    .setMessage("谢谢您对本软件的支持 \n\n开发者\n 一块小板子 2232442466 \n鸣谢\n 小青光 1664147500")
+                    .setTitle("关于『听筒播放』")
+                    .setMessage("我即使是死了,钉在在棺材里了,也要在墓里,用这腐朽的声带喊出：“我爱你。” \n\n作品本人仅发布于酷安（coolapk）。\n基于EarPlay(一块小板子 2232442466)，目前还在施工中。\n\n\n\ntumuyan，2018.9\n")
                     .setPositiveButton("确定", null)
                     .setNegativeButton("不确定", new DialogInterface.OnClickListener() { 
                         @Override 
@@ -246,7 +422,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         return super.onMenuItemSelected(featureId, item);
     }
 
-    public void mbar(float y) {
+/*    public void mbar(float y) {
         //1.设置属性的初始值和结束值
         final ValueAnimator mAnimator = ValueAnimator.ofFloat(ii.getY(), y);
         //2.为目标对象的属性变化设置监听器
@@ -260,7 +436,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         //3.设置动画的持续时间
         mAnimator.setDuration(250)
             .start();
-    }
+    }*/
 
     // 添加常驻通知
     public void setNotification() {
@@ -291,6 +467,8 @@ public class MainActivity extends Activity implements SensorEventListener {
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.cancel(0);
     }
+
+
 
 
 }
